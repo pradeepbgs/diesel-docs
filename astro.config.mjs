106ff49efa385f49414a930aed7dcbbfb4f58408
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-10-29 15:58:41 +0530

    building docs for our diesel

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Diesel',
+			social: {
+				github: 'https://github.com/pradeepbgs/diesel',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
