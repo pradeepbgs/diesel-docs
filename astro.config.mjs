b9dff0db504db89b2a93416e9f0991875a2b3650
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-10-30 16:42:20 +0530

    made router and ctx parts

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,16 +12,33 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Getting-Started',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{
+							label: 'Getting Started',
+							slug: 'getting-started/getting-started',
+						},
+						{
+							label: 'Router',
+							slug: 'getting-started/router',
+						},
+						{
+							label: 'Context',
+							slug: 'getting-started/ctx',
+						}
 					],
 				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', slug: 'guides/example' },
+				// 	],
+				// },
+				// {
+				// 	label: 'Reference',
+				// 	autogenerate: { directory: 'reference' },
+				// },
 			],
 		}),
 	],
